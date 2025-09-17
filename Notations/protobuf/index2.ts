cfab3a7abde72e79f036611b840c41496cc2c28b
Husnain Babar <husnain.babar.dev@gmail.com>
2025-09-17 17:14:05 +0500

    learn notation protobuf

--- Notations/protobuf/index2.ts
+++ Notations/protobuf/index2.ts
@@ -1,25 +1,30 @@
+// @ts-ignore
 import * as Schema from "./generated/employeee.js";
 
 console.log(Schema);
 
+// @ts-ignore
 const husnain = new Schema.Employees();
 husnain.title= "Husnain";
 husnain.salary= 1000;
 husnain.Residence= false;
 
 
+// @ts-ignore
 const husnain1 = new Schema.Employees();
 husnain1.title= "Husnain";
 husnain1.salary= 1000;
 husnain1.Residence= false;
 
 
+// @ts-ignore
 const husnain13 = new Schema.Employees();
 husnain13.title= "Husnain";
 husnain13.salary= 1000;
 husnain13.Residence= false;
 
 
+// @ts-ignore
 const husnain12 = new Schema.Employees();
 husnain12.title= "Husnain";
 husnain12.salary= 1000;
