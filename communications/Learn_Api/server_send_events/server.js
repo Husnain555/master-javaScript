933c77d143eaea620b2568fa56e61d482f31f4f7
Husnain Babar <husnain.babar.dev@gmail.com>
2025-09-18 14:02:54 +0500

    server send events

---
+++ communications/Learn_Api/server_send_events/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
+
+app.get("/stream", (req, res) => {
+    res.header("content-type", "text/event-stream");
+    // res.write("data: "+"Hello World!\n\n");
+    send(res);
+})
+let i = 0
+function send(res){
+    res.write("data: " + `${++i} Hello World!\n\n");`)
+    console.log(i,'i')
+    setTimeout(()=> send(res),1000)
+}
